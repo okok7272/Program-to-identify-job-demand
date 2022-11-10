3de2e1b0eb20bd5eeb9bcf86dd4b4aaa36c7afdb
okok7272 <kreuz1121@gmail.com>
2022-11-10 13:14:46 +0900

    백엔드 수정중

--- Data-Analysis/Back_End_collection.py
+++ Data-Analysis/Back_End_collection.py
@@ -7,24 +7,68 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 
 #https://www.jobkorea.co.kr/Search/?stext=PM&careerType=1&edu=0&Page_No=1&tabType=recruit
+#회사 수에 따라 페이지 수 설정 때문에 필요
+#first_url = 'https://www.jobkorea.co.kr/Search/?stext=AI&tabType=recruit&Page_No=1'
+
+page_order = 1
+
+#content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.list-filter-wrap > p > strong
+
+
 
 base_url = 'http://www.jobkorea.co.kr/Search/?stext={}&tabType=recruit&Page_No={}'
 
-#num
-#content > div > div > div.cnt-list-wrap.topLine > div > div.recruit-info > div.list-filter-wrap > p > strong
-#name
-##content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li:nth-child(1) > div > div.post-list-corp > a
-#채용형태
-#content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li:nth-child(1) > div > div.post-list-info > p.option > span:nth-child(3)
-#모집기간
-#content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li:nth-child(1) > div > div.post-list-info > p.option > span.date
+def page_number(keyword, page_num):
+    keyword = urllib.parse.quote(keyword)
+    url = base_url.format(keyword,page_num)
+    response = urllib.request.urlopen(url)
+    soup = bs(response,'html.parser')
+    text_num = soup.select_one("div.recruit-info > div.list-filter-wrap > p > strong").text
+    trans_num = re.sub(r'[^0-9]', '', text_num)
+    real_num = int(trans_num)
+    return real_num
+
 def crawl(keyword, page_num):
     keyword = urllib.parse.quote(keyword)
     url = base_url.format(keyword,page_num)
     response = urllib.request.urlopen(url)
     soup = bs(response,'html.parser')
     name = [element.text for element in soup.select("a.name")][:19]
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span.exp
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span.edu
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span:nth-child(3)
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span.loc.short
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span.loc.long
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > p.option > span.date
+    #content > div > div > div.cnt-list-wrap > div > div.recruit-info > div.lists > div > div.list-default > ul > li.list-post.active > div > div.post-list-info > a > strong
+    personal_history = [element.text for element in soup.select("div.post-list-info > p.option > span.exp")][:19]
+    edu= [element.text for element in soup.select("div.post-list-info > p.option > span.edu")][:19]
+    employment_type = [element.text for element in soup.select("div.post-list-info > p.option > span:nth-child(3)")][:19]
+    location = [element.text for element in soup.select("div.post-list-info > p.option > span.loc.long")][:19]
+    datetime = [element.text for element in soup.select("div.post-list-info > p.option > span.date")][:19]
     detail = [element.text for element in soup.select("div.post-list-info > a.title")][:19]
     detail = [element.replace("\n","").replace("\r","") for element in detail]
-    df = pd.DataFrame({'기업 이름' : name, '자세 내용' : detail})
+    df = pd.DataFrame({'회사명' : name, '채용제목' : detail, '근무지역' : location, '최소학력' : edu, '근무형태' : employment_type ,'경력' : personal_history , '모집기간' : datetime})
     return df
+
+#해당 함수 구조
+            # "회사명":COMPANY,
+            # "체용제목":TITLE,
+            # "임금형태":SAL_TMNM,
+            # "급여":SAL,
+            # "근무지역":REGION,
+            # "근무형태":HOLIDAY_TPNM,
+            # "최소학력":MIN_DEUBG,
+            # "경력":CAREER,
+#해당 구조와 비슷하게 데이터 받기 
+
+BE_df = pd.DataFrame(columns=['회사명', '채용제목', '근무지역', '최소학력', '근무형태', '경력', '모집기간'])
+
+def auto_crawling(keyword):
+    page_num = page_number(keyword, 1)
+    for i in range(page_num):
+        temp_df = crawl(keyword, i)
+        #페이지 켜지는 시간 넣기 sleep으로
+        BE_df.append(temp_df,sort=False, ignore_index = True)
+
+auto_crawling('백엔드')
